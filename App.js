a8cd4178d9bf3ac6b93fc77b3f298db5113ec5da
Thierry Ravelonjanahary <t.ravelonjanahary@gmail.com>
2023-07-07 15:31:44 +0200

    init application

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Weather's app using React Native</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  title: {
+    color: "#000",
+    fontWeight: "600"
+  }
+});
